2c90735bacfd69794c0dd00d529cbb3c535d4f8f
Adam Huganir <adam@huganir.com>
2021-12-04 12:46:01 -0500

    changed to list to make sure that multiline imports work, as well as adding support for multiline imports

--- src/hugtools/jupyter/__init__.py
+++ src/hugtools/jupyter/__init__.py
@@ -2,7 +2,7 @@ import json
 from typing import Set
 
 
-def get_all_notebook_imports(notebook_filename: str, verbose: bool = True) -> Set[str]:
+def get_all_notebook_imports(notebook_filename: str, verbose: bool = True, include_first_cell=True) -> List[str]:
     """Parse a jupyter notebook and look for imports that have been used and collate them together to help
     with keeping everything runnable from the top
 
@@ -11,16 +11,28 @@ def get_all_notebook_imports(notebook_filename: str, verbose: bool = True) -> Se
         verbose (bool): whether to print to stdout
 
     Returns:
-        set[str] : the import statements found deduplicated into a set
+        list[str] : the import statements found in order
 
     """
     with open(notebook_filename) as f:
         ipynb = json.load(f)
+
+    parens = 0
     imports = []
-    for cell in ipynb["cells"]:
+    for cell_index, cell in enumerate(ipynb["cells"]):
+        if not include_first_cell:
+            continue
         for line in cell["source"]:
-            if line.startswith("import") or (line.startswith("from") and "import in line"):
-                imports.append(line)
+            if line.startswith("import") or (line.startswith("from") and "import" in "line"):
+                if "(" in line:
+                    parens += 1
+                imports.append(line.rstrip())
+            elif parens > 0:
+                imports.append(line.rstrip())
+                if ")" in line:
+                    parens -= 1
+
     if verbose:
         print("\n".join(imports))
-    return set(imports)
+
+    return imports
